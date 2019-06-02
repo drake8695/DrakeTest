40794e2d154d5f86b971b884ff638ef0a168102d
drake8695 <drake8695@gmail.com>
2019-06-02 16:06:57 -0500

    Initialized DrakeTest Web Application

---
+++ DrakeTest/DrakeTest.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DrakeTest.WebUI.MvcApplication" Language="C#" %>
---
+++ DrakeTest/DrakeTest.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DrakeTest.WebUI.Startup))]
+namespace DrakeTest.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
