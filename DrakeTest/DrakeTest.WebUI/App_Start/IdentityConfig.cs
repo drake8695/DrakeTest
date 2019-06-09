5664e74b5039be4d1ce5f01655ed147ba674ad3c
drake8695 <drake8695@gmail.com>
2019-06-09 00:43:56 -0500

    Worked on user registration and email verification. -cant get pic from local folder to show in email.

--- DrakeTest/DrakeTest.WebUI/App_Start/IdentityConfig.cs
+++ DrakeTest/DrakeTest.WebUI/App_Start/IdentityConfig.cs
@@ -10,16 +10,56 @@ using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
+using SendGrid;
+using System.Net;
+using System.Configuration;
+using System.Diagnostics;
 using DrakeTest.WebUI.Models;
+
 
 namespace DrakeTest.WebUI
 {
     public class EmailService : IIdentityMessageService
     {
-        public Task SendAsync(IdentityMessage message)
+        public async Task SendAsync(IdentityMessage message)
         {
             // Plug in your email service here to send an email.
-            return Task.FromResult(0);
+            await configSendGridasync(message);
+        }
+        // Use NuGet to install SendGrid (Basic C# client lib) 
+        private async Task configSendGridasync(IdentityMessage message)
+        {
+            var myMessage = new SendGridMessage();
+            myMessage.AddTo(message.Destination);
+            myMessage.From = new System.Net.Mail.MailAddress(
+                                "drake@draketest.com", "Drake Shaw");
+            myMessage.Subject = message.Subject;
+
+
+            
+
+
+            myMessage.Text = message.Body;
+            myMessage.Html = "<html><body><a href='' title='My Website'><img src='../Content/Pictures/SmallLogo.png' alt='My Image' border='0' /></a><br /><h1>My E-mail Title</h1>E-mail content.</body></html>" + @message.Body;
+            
+            var credentials = new NetworkCredential(
+                       ConfigurationManager.AppSettings["mailAccount"],
+                       ConfigurationManager.AppSettings["mailPassword"]
+                       );
+
+            // Create a Web transport for sending email.
+            var transportWeb = new Web(credentials);
+
+            // Send the email.
+            if (transportWeb != null)
+            {
+                await transportWeb.DeliverAsync(myMessage);
+            }
+            else
+            {
+                Trace.TraceError("Failed to create Web transport.");
+                await Task.FromResult(0);
+            }
         }
     }
 
