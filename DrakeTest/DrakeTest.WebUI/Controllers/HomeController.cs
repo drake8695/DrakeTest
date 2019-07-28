340a5b18ef8e74949e0bdff63e929b34ba6350a1
drake8695 <drake8695@gmail.com>
2019-07-27 21:16:51 -0500

    Upgraded from bootstrap 3 to bootstrap 4. Need to tweek things around to work for Browsers: Edge and internet explorer.

--- DrakeTest/DrakeTest.WebUI/Controllers/HomeController.cs
+++ DrakeTest/DrakeTest.WebUI/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ namespace DrakeTest.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        [Authorize]
         public ActionResult Index()
         {
             return View();
